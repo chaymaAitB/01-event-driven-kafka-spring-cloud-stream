70aa90f6961f00cc5065241cb59dc8cf81f94341
chayma <chayma@localhost.localdomain>
2025-10-08 13:33:47 +0100

    Initial commit

---
+++ src/main/java/com/example/kafkaspingcloudstream/KafkaSpingCloudStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaspingcloudstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpingCloudStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpingCloudStreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkaspingcloudstream/KafkaSpingCloudStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaspingcloudstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpingCloudStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
